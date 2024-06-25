01bb19a815df33f3ec8f02bbc821d324351cf269
jacksonbrito <jackson.brito03@gmail.com>
2024-06-25 15:28:47 -0300

    feat: inicio de um sonho

---
+++ src/test/java/com/br/pix_manage_key_microservice/PixManageKeyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pix_manage_key_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixManageKeyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
